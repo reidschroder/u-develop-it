4471544148138d0f47981472545b6e53362dea90
Reid Schroder <reidschroder2@gmail.com>
2022-02-28 18:23:44 -0500

    query the database to test connection in server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,23 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username
+        user: 'root',
+        //============ADD PASSWORD FOR CONNECTION TO ELECTION DATABASE=============================
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (Nopt found)
 app.use((req,res) => {
